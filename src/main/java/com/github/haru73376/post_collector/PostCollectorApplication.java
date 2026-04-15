98334275d7085d873c92779f56f54a62b2c61e78
Haruka Nakajima <haruca058729@gmail.com>
2026-04-14 20:13:36 -0400

    feat: initial project setup

---
+++ src/main/java/com/github/haru73376/post_collector/PostCollectorApplication.java
@@ -0,0 +1,13 @@
+package com.github.haru73376.post_collector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostCollectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/haru73376/post_collector/PostCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.haru73376.post_collector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostCollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
